e7d949821107c3d39449416da6dc83d2be19793d
Skeler667 <loloalilqwertykop@gmail.com>
2023-05-26 13:51:18 +0300

    fix login page validation step 2

--- frontend/src/Components/LoginPage.jsx
+++ frontend/src/Components/LoginPage.jsx
@@ -4,10 +4,11 @@ import * as Yup from 'yup';
 import { Link, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import {
-  Container, Button, Form, Row, Col, FloatingLabel, FormText,
+  Container, Button, Form, Row, Col, FormText,
 } from 'react-bootstrap';
 import axios from 'axios';
 import useAuth from '../hooks/useAuth.hook';
+import Spinner from 'react-bootstrap/Spinner';
 
 const LoginPage = () => {
   const { t } = useTranslation();
@@ -45,61 +46,65 @@ const LoginPage = () => {
   return (
     <Container>
       <Row>
-        <Col className="mt-5 text-center mx-auto" md={6}>
+        <Col className="col-9 m-auto mt-5">
           <h1 className="text-center">{t('login.title')}</h1>
-          <Form onSubmit={formik.handleSubmit} className="mt-4">
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <FloatingLabel label={t('login.username')}>
-                <Form.Control
-                  autoComplete="off"
-                  name="username"
-                  value={formik.values.username}
-                  onBlur={formik.handleBlur('username')}
-                  onChange={formik.handleChange}
-                  className={formik.errors.username && formik.touched.username ? 'is-invalid' : ''}
-                  type="text"
-                />
-              </FloatingLabel>
+          <Form
+            onSubmit={formik.handleSubmit}
+          >
+            <Form.Group className="mb-3 form-floating">
+              <Form.Control
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                type="text"
+                id="floatingLogin"
+                name="username"
+                disabled={formik.isSubmitting}
+                placeholder={t('login.username')}
+                autoComplete="off"
+                className={formik.errors.username && formik.touched.username ? 'is-invalid' : ''}
+              />
+              <Form.Label htmlFor="floatingLogin">{t('login.username')}</Form.Label>
               {
-                formik.errors.username
-                && formik.touched.username
-                && <FormText className="feedback text-danger mt-3">{t(formik.errors.username)}</FormText>
+              formik.errors.username
+              && formik.touched.username
+              && <FormText className="feedback text-danger mt-3">{t(formik.errors.username)}</FormText>
+            }
+
+            </Form.Group>
+            <Form.Group className="mb-3 form-floating">
+              <Form.Control
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                type="password"
+                id="floatingPassword"
+                name="password"
+                autoComplete="off"
+                disabled={formik.isSubmitting}
+                placeholder={t('login.password')}
+                className={formik.errors.password && formik.touched.password ? 'is-invalid' : ''}
+              />
+              <Form.Label htmlFor="floatingPassword">{t('login.password')}</Form.Label>
+              {
+                formik.errors.password
+                && formik.touched.password
+                && <FormText className="feedback text-danger mt-3">{t(formik.errors.password)}</FormText>
               }
-              <FormText className="feedback text-danger mt-3">{authError}</FormText>
+              <FormText className="feedback text-danger mt-3">{t(authError)}</FormText>
             </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <FloatingLabel label={t('login.password')}>
-                <Form.Control
-                  autoComplete="off"
-                  name="password"
-                  value={formik.values.password}
-                  onBlur={formik.handleBlur('username')}
-                  onChange={formik.handleChange}
-                  type="password"
-                  className={formik.errors.password && formik.touched.password ? 'is-invalid' : ''}
-                />
-              </FloatingLabel>
-              {
-                  formik.errors.password
-                  && formik.touched.password
-                  && <FormText className="feedback text-danger mt-3">{t(formik.errors.password)}</FormText>
-                }
-              <FormText className="feedback text-danger mt-3">{authError}</FormText>
-
-            </Form.Group>
-            <div className="d-grid gap-2">
-              <Button size="lg" variant="primary" type="submit">
-                {t('login.submit')}
-              </Button>
-              <p className="mt-3">
-                {t('login.hasAccount')}
-                <Link style={{ marginLeft: 5 }} to="/signup">{t('signup.title')}</Link>
-              </p>
-            </div>
+            <Button
+              disabled={formik.isSubmitting}
+              variant="primary"
+              type="submit"
+            >
+              {formik.isSubmitting && <Spinner size="sm" />}
+              {t('login.submit')}
+            </Button>
           </Form>
+          <p className="mt-3">
+            {t('login.hasAccount')}
+            <Link style={{ marginLeft: 5 }} to="/signup">{t('signup.title')}</Link>
+          </p>
         </Col>
-
       </Row>
     </Container>
   );
